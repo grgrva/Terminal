8ddbd6b96171887f7e585f83408571814802068e
grgrva <grgrva_elena@gmail.com>
2023-07-14 14:02:16 +0400

    add all hw

---
+++ script_terminal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir HW_QA
+cd HW_QA/
+mkdir dir1 dir2 dir3
+cd dir1/
+touch 1.txt 2.txt 3.txt 4.json 5.json
+mkdir dir1.1 dir1.2 dir1.3
+ls -la
+pwd
+mv dir1/2.txt dir1/5.json HW_QA/dir3
